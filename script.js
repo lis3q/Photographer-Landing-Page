1314007e3e625390bdd5d5ae40a050e1b1105ba4
lis3q . <lis3qx@gmail.com>
2024-02-19 20:00:41 +0100

    I added texts, created a function that changes the link to active when scrolling the page, added wave svg to the hero section and added a favicon

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
+// Sticky navbar
 const navbar = document.querySelector(".navbar");
 window.addEventListener("scroll", () => {
     navbar.classList.toggle("navbar-sticky", window.scrollY > 0)
 })
+
+// Link active when scrolling
+const sections = document.querySelectorAll("section");
+const navlink = document.querySelectorAll(".nav-link");
+window.onscroll = () => {
+    sections.forEach(section => {
+        let top = window.scrollY;
+        let offset = section.offsetTop - 150;
+        let height = section.offsetHeight;
+        let id = section.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navlink.forEach(link => {
+                link.classList.remove("active");
+                document.querySelector('.navbar-nav a[href*=' + id + ']').classList.add("active");
+            })
+        }
+    })
+}
